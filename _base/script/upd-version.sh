422ebcdb486bcade4472bcb7d1f19ed26696849e
Ed <estlouis@ximasoftware.com>
2016-07-05 15:00:34 -0600

    Updated version (for specific Xima build) and commented out bad test.

--- _base/script/upd-version.sh
+++ _base/script/upd-version.sh
@@ -8,11 +8,11 @@ fi
 echo "Updating version from $2 to $3 in directory $1 and subfolders"
 
 TFILE="/tmp/out.tmp.$$"
-for filename in $(grep -r "$2" --include "*.java" --include "*.htm*" --include "*.xml" --exclude-dir "apidocs" --exclude-dir "target" --exclude-dir "test-output" $1|cut -f 1 -d :)
+for filename in $(/bin/grep -r "$2" --include "*.java" --include "*.htm*" --include "*.xml" --exclude-dir "apidocs" --exclude-dir "target" --exclude-dir "test-output" $1|/bin/cut -f 1 -d :)
 do
   if [ -f $filename -a -r $filename ]; then
     #/bin/cp -f $filename ${filename}.old
-    sed "s/$2/$3/g" "$filename" > $TFILE && mv $TFILE "$filename"
+    /bin/sed "s/$2/$3/g" "$filename" > $TFILE && mv $TFILE "$filename"
   else
     echo "Error: Cannot read $filename"
   fi
